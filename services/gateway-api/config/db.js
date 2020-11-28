94d93b884c156f322957d1cf8b72a835c09f5e8a
Martin <mr.krustev1994@gmail.com>
2020-11-28 13:16:17 +0200

    Fix db config file name. Update reference. Init connection.

--- services/gateway-api/config/db.js
+++ services/gateway-api/config/db.js
@@ -14,3 +14,5 @@ const connectDB = async () => {
         process.exit(1)
     }
 }
+
+module.exports = connectDB
--- services/gateway-api/index.js
+++ services/gateway-api/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const app = express()
 const dotenv = require('dotenv')
+const connectDB = require('./config/db ')
 
 // Env variables.
 dotenv.config({
         path: './config/config.env'
 })
 const PORT =  process.env.PORT || 3000;
+
+// Connect to mongo.
+connectDB()
+
+const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
