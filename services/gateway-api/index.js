af2bfc8d9debc6ac4641be70d42d6add65347c6c
Martin <mr.krustev1994@gmail.com>
2020-11-28 15:04:12 +0200

    Add parser for JSON and url encoded.

--- services/gateway-api/index.js
+++ services/gateway-api/index.js
@@ -5,18 +5,21 @@ const routes = require('./routes/index')
 
 // Env variables.
 dotenv.config({
-        path: './config/config.env'
+    path: './config/config.env'
 })
-const PORT =  process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to mongo.
 connectDB()
 
 const app = express()
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-// 
+// Routes
 routes(app)
 
-app.listen(PORT,  () => {
+app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 });
