1de60ac41ff686c7e07086811ebc29e70ab5dffa
Martin <mr.krustev1994@gmail.com>
2020-11-28 15:04:39 +0200

    Implement creation of gateways.

--- services/gateway-api/routes/controllers/gateways.js
+++ services/gateway-api/routes/controllers/gateways.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
+const Gateway = require('../../models/Gateway')
 
 
-router.get('/gateways', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Reached gateways.')
 });
 
-router.post('/gateways', (req, res) => {
 
+// router.get('/{id}', (req, res) => {
+//     res.send('Reached gateways.')
+// });
+
+router.post('/', async (req, res, next) => {
+    try {
+        const result = await Gateway.create(req.body)
+        res.status(201).send(result)
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
 });
 
 module.exports = router;
