c7afcc3efa06d0f652cb5ec5bc36f939130e021d
Martin <mr.krustev1994@gmail.com>
2020-11-28 17:53:48 +0200

    Update dependencies.

--- services/gateway-api/config/db.js
+++ services/gateway-api/config/db.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-             useNewUrlParser: true,
-             useUnifiedTopology: true,
-             useFindAndModify: false
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         })
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch(err) {
+    } catch (err) {
         console.log(err)
         process.exit(1)
     }
