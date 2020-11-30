cb63755361aa9106827119ef9985cc417e5cac1a
Martin <mr.krustev1994@gmail.com>
2020-11-30 17:35:18 +0200

    Implement API consumer methods.

--- client/src/app/services/gateway.service.ts
+++ client/src/app/services/gateway.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Gateway } from '../models/gateway';
+import { Peripheral } from '../models/peripheral';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +17,22 @@ export class GatewayService {
   }
 
   getAllGateways(): Observable<Gateway[]> {
-    return this.httpClient.get<Gateway[]>(this.GATEWAY_BASE_URL + '/gateways', { headers: this.headers });
+    return this.httpClient.get<Gateway[]>(`${this.GATEWAY_BASE_URL}/gateways`, { headers: this.headers });
   }
 
   getGatewayById(id: string): Observable<Gateway> {
-    return this.httpClient.get<Gateway>(this.GATEWAY_BASE_URL + '/gateways/' + id, { headers: this.headers });
+    return this.httpClient.get<Gateway>(`${this.GATEWAY_BASE_URL}/gateways/${id}`, { headers: this.headers });
+  }
+
+  addGateway(gateway: Gateway): Observable<Gateway> {
+    return this.httpClient.post<Gateway>(`${this.GATEWAY_BASE_URL}/gateways/`, gateway, { headers: this.headers });
+  }
+
+  addPeripheralDevice(gatewayID: string, peripheral: Peripheral): Observable<Peripheral> {
+    return this.httpClient.post<Peripheral>(`${this.GATEWAY_BASE_URL}/gateways/${gatewayID}/peripherals`, peripheral, { headers: this.headers });
+  }
+
+  removePeripheralDeviceByID(gatewayID: string, peripheralDeviceID: string): Observable<any> {
+    return this.httpClient.delete(`${this.GATEWAY_BASE_URL}/gateways/${gatewayID}/peripherals/${peripheralDeviceID}`, { headers: this.headers });
   }
 }
