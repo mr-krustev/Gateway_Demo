71506b50fb74d45216995ee4d1c35394fab371bf
Martin <mr.krustev1994@gmail.com>
2020-11-30 17:36:34 +0200

    Move root component. Update routes and imports.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/partials/dashboard/dashboard.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: DashboardComponent },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' }
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Services
+import { GatewayService } from './services/gateway.service';
+
+// Root
+import { AppComponent } from './components/core/root/app.component';
+import { FooterComponent } from './components/core/footer/footer.component';
+import { ToolbarComponent } from './components/core/toolbar/toolbar.component';
+
+// Pages
+import { HomeComponent } from './components/home/home.component';
+
+// Partials
+import { DashboardComponent } from './components/partials/dashboard/dashboard.component';
+import { GatewayCardComponent } from './components/partials/gateway-card/gateway-card.component';
+
+// Dialogs
+import { AddPeripheralDialogComponent } from './components/partials/dialogs/add-peripheral-dialog/add-peripheral-dialog.component';
+import { ConfirmationDialogComponent } from './components/partials/dialogs/confirmation-dialog/confirmation-dialog.component';
+import { AddGatewayDialogComponent } from './components/partials/dialogs/add-gateway-dialog/add-gateway-dialog.component';
+
+// Pipes
+import { JoinPipe } from './utilities/pipes/join.pipe';
+import { RegexSplitPipe } from './utilities/pipes/regex-split.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    ToolbarComponent,
+    FooterComponent,
+    GatewayCardComponent,
+    AddPeripheralDialogComponent,
+    ConfirmationDialogComponent,
+    AddGatewayDialogComponent,
+    JoinPipe,
+    RegexSplitPipe
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    MaterialModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    GatewayService
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AddPeripheralDialogComponent,
+    ConfirmationDialogComponent,
+    AddGatewayDialogComponent
+  ]
 })
 export class AppModule { }
